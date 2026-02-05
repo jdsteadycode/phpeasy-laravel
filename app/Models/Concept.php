e7290093c9ab9c191dcfd0eb44de3d84fc77c452
jdsteadycode <desaijeet11@gmail.com>
2026-02-05 19:23:58 +0530

    Added relationship for Concept

--- app/Models/Concept.php
+++ app/Models/Concept.php
@@ -1,10 +1,28 @@
 <?php
 
+// package path
 namespace App\Models;
 
+// path to Model Class
 use Illuminate\Database\Eloquent\Model;
 
+// path to ConceptContent Class
+use App\Models\ConceptContent;
+
+// UDC Concept inheriting from Model Class
 class Concept extends Model
 {
-    //
+    // to be filled..
+    public $fillable = [
+        'description',
+        'example_code',
+        'notes'
+    ];
+
+    // () -> get content
+    public function content()
+    {
+        // i.e., one concept has exactly one content..
+        return $this->hasOne(ConceptContent::class);
+    }
 }
